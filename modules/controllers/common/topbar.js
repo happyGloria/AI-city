c8623253170bee5a7e18462374fe9ac0a6d4a64e
paul <wuweijun@lingdanet.com>
2018-05-11 11:00:04 +0800

    实有力量/实有安防措施/实有人员 调接口  推出登陆

--- modules/controllers/common/topbar.js
+++ modules/controllers/common/topbar.js
@@ -17,6 +17,75 @@ define(['controllers/controllers', 'jquery', 'config/configFile', 'layer', 'noti
                 $scope.hideSecondMenus=function () {
                     $scope.showSecondMenusHover = false
                 }
+                $scope.msg = "确定要退出【奉贤社会面智能安防平台】吗？";
+                //退出登录弹出框
+                var logOut = '';
+                $scope.logout = function () {
+                    logOut = layer.open({
+                        type: 1,
+                        title: ' ',
+                        shadeClose: true,
+                        maxmin: false,
+                        area: ['4.1rem', '2.84rem'],
+                        content: '',
+                        success: function (layero) { //层销毁后触发的回调
+                            angular.element(layero).find('.layui-layer-content').append($compile
+
+                                ($('#layer-login-out').html())($scope));
+                        },
+                        yes: function (index, layero) {
+                            $scope.msg = "确定要退出【智慧安防社区系统】吗？";
+
+                        },
+                        cancel: function (index, layero) {
+                            $scope.msg = "确定要退出【智慧安防社区系统】吗？";
+                            layer.close(logOut);
+                        }
+                    });
+                };
+                $scope.loginLayer = null;
+                $scope.openLog = function () {
+                    debugger
+                    $scope.loginLayer = layer.open({
+                        type: 1,
+                        title: '',
+                        maxmin: false,
+                        area: ['3rem', '2rem'],
+                        shadeClose: true,
+                        content: '', //$('#layer-org-create'),
+                        success: function (layero) {
+                            angular.element(layero).find('.layui-layer-content').addClass('layer-drop').append($compile($('#layer-login-out').html())($scope));
+
+                        },
+                        end: function () { //层销毁后触发的回调
+
+                        },
+                        yes: function (index, layero) { //确定
+                            //do something
+                        },
+                        cancel: function (index, layero) { //取消
+                            //do something
+                        }
+                    });
+                }
+                //退出登录
+                $scope.goLogOut = function () {
+                    $http.get('/zhsq/system/logout?account=admin').then(function (resp) {
+                        debugger
+                        if (resp.data.resultCode == 200 && resp.data.data.status == 'success') {
+                            layer.close($scope.loginLayer);
+                            setTimeout(function () {
+                                window.location.href = "/#/index/login/";
+                            }, 200)
+                        }
+                    }).catch(function () { }).finally(function () { });
+
+                };
+
+                //取消退出登录
+                $scope.closeOut = function () {
+                    layer.close($scope.loginLayer);
+                };
 
 			}
 		];
--- modules/controllers/community/communityPanelCtrl.js
+++ modules/controllers/community/communityPanelCtrl.js
@@ -3,8 +3,8 @@
  * 2018/05/08
  */
 define(
-    ['app', 'controllers/controllers', 'jquery', '/modules/config/configFile.js', '/modules/config/basicConfig.js','/modules/config/echartsConfig.js', 'notify', 'echarts-dark', 'controllers/community/2dMapCtrl', 'config/common'],
-	function(app, controllers, $, configFile, basicConfig, echartsConfig, notify, dark, dMapCtrl, common) {
+    ['app', 'controllers/controllers', 'jquery', '/modules/config/configFile.js', '/modules/config/basicConfig.js', '/modules/config/echartsConfig.js', 'notify', 'echarts-dark', 'controllers/community/2dMapCtrl', 'config/common','yituFace'],
+    function (app, controllers, $, configFile, basicConfig, echartsConfig, notify, dark, dMapCtrl, common, yituFace) {
 		var communityPanelCtrl = [
             '$scope',
             '$state',
@@ -52,6 +52,7 @@ define(
                                 ("00" + o[k]).substr(("" + o[k]).length));
                     return format;
                 }
+                /* 实时时间 */
                 $scope.nowTimeInt = $interval(function () {
                     var d2 = new Date();
                     var day = d2.getDay();
@@ -269,64 +270,133 @@ define(
                     ECharts.setOption(option);
                 }
                 // 1. 一标六实数据
+                var villageCode = $stateParams.villageCode || '';
                 // 2.1 年龄分布
                 /* 实有力量~~~~~~~~~~~· */
                 //1)实有力量在线统计
-                // queryRealPower()
-                // function queryRealPower() {
-                //     communityAllService.realPower().then(function (resp) {
-                //         if (resp.resultCode == '200') {
-                //             var powerData = resp.data
-                //             var powerOption = echartsConfig.triangleEcharts(['警员', '居委干部', '楼组长', '志愿者', '保安', '保洁', '保绿', '快递人员'], [powerData.jy, powerData.jwgb, powerData.lzz, powerData.zyz, powerData.ba, powerData.bj, powerData.bl, powerData.kd])
-                //             setEchart("PowerAnalysis", powerOption)
-                //         }
-                //     }).catch(function () { }).finally(function () { });
-                // }
-                var powerOption = echartsConfig.triangleEcharts(['警员', '居委干部', '楼组长', '志愿者', '保安', '保洁', '保绿', '快递人员'], [1, 2, 3, 4, 5, 6, 7, 8])
-                setEchart("PowerAnalysis", powerOption, function (params) {
-                    //点击图标跳转页面
-                    var key = "resident_";
-                    localStorage.setItem(key, JSON.stringify(params.dataIndex + 1 + ""));
-                    var newurl = window.location.href.split("/#")[0] + "#/index/factPower/";
-                    window.open(newurl);
-                })
+                queryRealPower()
+                function queryRealPower() {
+                    communityAllService.realPower().then(function (resp) {
+                        if (resp.resultCode == '200') {
+                            var powerData = resp.data
+                            var powerOption = echartsConfig.triangleEcharts(['警员', '居委干部', '楼组长', '志愿者', '保安', '保洁', '保绿', '快递人员'], [powerData.jy||0, powerData.jwgb||0, powerData.lzz||0, powerData.zyz||0, powerData.ba||0, powerData.bj||0, powerData.bl||0, powerData.kd||0])
+                            setEchart("PowerAnalysis", powerOption)
+                        }
+                    }).catch(function () { }).finally(function () { });
+                }
+                // var powerOption = echartsConfig.triangleEcharts(['警员', '居委干部', '楼组长', '志愿者', '保安', '保洁', '保绿', '快递人员'], [1, 2, 3, 4, 5, 6, 7, 8])
+                // setEchart("PowerAnalysis", powerOption, function (params) {
+                //     //点击图标跳转页面
+                //     var key = "resident_";
+                //     localStorage.setItem(key, JSON.stringify(params.dataIndex + 1 + ""));
+                //     var newurl = window.location.href.split("/#")[0] + "#/index/factPower/";
+                //     window.open(newurl);
+                // })
                 //实有安防设施分析
-                moreService.queryFacilityList({ villageCode: '' }).then(function (resp) {
-                    console.log(resp.data)
+                moreService.queryFacilityList({ villageCode: villageCode }).then(function (resp) {
                     var dataArr = []
                     var nameArr = []
-                    resp.data.forEach(function (v, k) {
+                     resp.data&&resp.data.forEach(function (v, k) {
                         dataArr.push(v.num)
                         if (v.name === 'WiFi探针') {
                             v.name = 'WiFi\n探针'
-                        } else if (v.name.length > 2) {
-                            v.name = v.name.substr(0, 2) + '\n' + v.name.substr(2, a.length - 2)
+                        } else if (v.name.length > 3) {
+                            v.name = v.name.substr(0, 2) + '\n' + v.name.substr(2, v.name.length - 2)
                         }
-                        dataArr.push(v.name)
+                        nameArr.push(v.name)
                     })
+                    var SafeOption = echartsConfig.triangleEcharts(nameArr, dataArr)
+                    setEchart("SafeAnalysis", SafeOption)
                 })
-                var SafeOption = echartsConfig.triangleEcharts(['窖井盖', '微型\n消防站', '消防栓', '电壶', '烟感', '车辆\n卡口', '人脸\n卡口', 'WIFI\n探针', '门禁', '摄像机'], ['170', '40', '178', '98', '74', '111', '100', '28', '144', '56'])
-                setEchart("SafeAnalysis", SafeOption)
-				/* 实有人员分析 */,
-                    $scope.peopleTabAction = 'age';
+                /* 实有人员分析 */
+                $scope.peopleTabAction = 'age';
                 $scope.changePeopleTab = function (val) {
                     $scope.peopleTabAction = val;
                 }
-                //年龄分布
-                var PeopleOption = echartsConfig.pieEcharts(['1', '2', '3', '4', '5'], ['1-16岁', '17-40岁', '41-60岁', '61-80岁', '80岁以上'], [0, 0, 0, 0, 0])
-                setEchart("ageAnalysis", PeopleOption)
-                //性别与户籍
-                /* 性别 */
-                var SexOption = echartsConfig.pieEcharts(['男', '女'], ['1', '2'], [45000, 55000])
-                SexOption.title.text = '性别\n比例',
+                function initiInside() {
+                    //查询实有人口类别接口
+                    var req = {
+                        "villageCode": villageCode
+                    }
+                    var resultData;
+                    moreService.queryRealPeopleNum(req).then(function (resp) {
+                        resultData = resp;
+                        yituFace.yitu_dossier("incoming_dossier", [villageCode], function (data) {
+                            if (data.statistic_info.length > 0) {
+                                resultData.data.peopleRecord.peopleDiscoveryCount = data.statistic_info[0].delta_num || 0;
+                            }
+                            yituFace.yitu_dossier("leaving_dossier", [villageCode], function (data) {
+                                if (data.statistic_info.length > 0) {
+                                    resultData.data.peopleRecord.peopleLeaveCount = data.statistic_info[0].delta_num || 0;
+                                }
+                                afterGetData(resp);
+                            });
+                        });
+                    }).catch(function () { }).finally(function () {
+                        afterGetData(resultData);
+                    });
+
+                };
+                initiInside()
+
+                function afterGetData(data) {
+                    console.log(data)
+                    //年龄
+                    var ageCon = ["age1", "age2", "age3","age4","age5"]
+                    var ageConfig = {
+                        "age1": "1-16岁",
+                        "age2": "17-40岁",
+                        "age3": "41-60岁",
+                        "age4": "61-79岁",
+                        "age5": "80以上"
+                    };
+                    var ageData = [];
+                    var ageName = [];
+                    var ageIndex=[]
+                    ageCon.forEach(function (v,k) {
+                        ageData.push(data.data.ageRecord[v])
+                        ageName.push(ageConfig[v])
+                        ageIndex.push(k+1)
+                    })
+                    //年龄分布
+                    var PeopleOption = echartsConfig.pieEcharts(ageIndex, ageName, ageData)
+                    setEchart("ageAnalysis", PeopleOption)
+                    /* 性别与户籍 */
+                    //性别 
+                    var sexData = [];
+                    var sexName = [];
+                    var sexIndex = [];
+                    data.data.genderRecord.filter(function (v) {
+                        return v.name!=="未知"
+                    }).forEach(function (v,k) {
+                        sexData.push(v.num)
+                        sexName.push(v.name)
+                        sexIndex.push(k+1)
+                    })
+                    var SexOption = echartsConfig.pieEcharts(sexName, sexIndex, sexData)
+                    SexOption.title.text = '性别\n比例',
                     setEchart("sexAnalysis", SexOption)
-                /* 户籍 */
-                var HujiOption = echartsConfig.pieEcharts(['来沪人员', '户籍人员', '外籍人员'], ['1', '2', '3'], [18000, 73000, 9000])
-                HujiOption.title.text = '户籍\n比例',
+                    //户籍
+                    var peopleTypeData = [];
+                    var peopleTypeName = [];
+                    var peopleTypeIndex = [];
+                    data.data.peopleTypeRecord.forEach(function (v, k) {
+                        peopleTypeData.push(v.num)
+                        peopleTypeName.push(v.name)
+                        peopleTypeIndex.push(k + 1)
+                    })
+                    // 户籍
+                    var HujiOption = echartsConfig.pieEcharts(peopleTypeName, peopleTypeIndex, peopleTypeData)
+                    HujiOption.title.text = '户籍\n比例',
                     setEchart("hujiAnalysis", HujiOption)
+                }
+
+                
+
+
                 // 人员性质
-                PeopleOption.title.text = '人员\n性质',
-                    setEchart("personnelAnalysis", PeopleOption)
+                // PeopleOption.title.text = '人员\n性质',
+                //     setEchart("personnelAnalysis", PeopleOption)
 
                 //底部面板切换
                 $scope.bottomTabAction = 'face';
